33ac2d444748a30b8d303c3e6d3c33bab06887ea
andrearuiz2005 <158847386+andrearuiz2005@users.noreply.github.com>
2024-08-22 17:19:16 -0600

    trabajo

---
+++ src/trabajo/TestMiFile.java
@@ -0,0 +1,113 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabajo;
+
+
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ar466
+ */
+
+public class TestMiFile {
+
+    static MiFile mf = new MiFile();
+    static Scanner lea = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int opcion = 0;
+        do {
+            System.out.println("\nMENU\n");
+            System.out.println("1- Set el archivo / folder.");
+            System.out.println("2- Ver informacion.");
+            System.out.println("3- Crear un archivo.");
+            System.out.println("4- Crear un Folder.");
+            System.out.println("5- Borrar.");
+            System.out.println("6- CMD - DIR");
+            System.out.println("7- Tree");
+            System.out.println("8- Escribir en el archivo.");
+            System.out.println("9- Leer el archivo.");
+            System.out.println("10- Salir.");
+            System.out.println("Escoja una opcion: ");
+            try {
+                opcion = lea.nextInt();
+                lea.nextLine();  // Consumir el salto de línea
+                switch (opcion) {
+                    case 1:
+                        set();
+                        break;
+                    case 2:
+                        mf.Info();
+                        break;
+                    case 3:
+                        mf.crearFile();
+                        break;
+                    case 4:
+                        mf.crearFolder();
+                        break;
+                    case 5:
+                        mf.borrar();
+                        break;
+                    case 6:
+                        mf.dir();
+                        break;
+                    case 7:
+                        mf.tree();
+                        break;
+                    case 8:
+                        escribirArchivo();
+                        break;
+                    case 9:
+                        leerArchivo();
+                        break;
+                    case 10:
+                        System.out.println("Saliendo...");
+                        break;
+                    default:
+                        System.out.println("Opción no válida. Intente de nuevo.");
+                        break;
+                }
+            } catch (InputMismatchException e) {
+                lea.nextLine();  // Consumir la entrada inválida
+                System.out.println("Por favor ingrese una opción correcta.");
+            } catch (NullPointerException e) {
+                System.out.println("Debes seleccionar la opción 1 por lo menos una vez.");
+            } catch (IOException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        } while (opcion != 10);
+    }
+
+    private static void set() {
+        System.out.println("Direccion: ");
+        mf.setFile(lea.next());
+    }
+
+    private static void escribirArchivo() throws IOException {
+        System.out.println("Escriba el contenido que desea agregar o reemplazar:");
+        String contenido = lea.nextLine();
+        System.out.println("¿Desea reemplazar el contenido existente o agregar al final? (R/A):");
+        String opcion = lea.next();
+        lea.nextLine();  // Consumir el salto de línea
+        if (opcion.equalsIgnoreCase("R")) {
+            mf.escribirReemplazar(contenido);
+            System.out.println("Contenido reemplazado exitosamente.");
+        } else if (opcion.equalsIgnoreCase("A")) {
+            mf.escribirAgregar(contenido);
+            System.out.println("Contenido agregado exitosamente.");
+        } else {
+            System.out.println("Opción no válida.");
+        }
+    }
+
+    private static void leerArchivo() throws IOException {
+        String contenido = mf.leerArchivo();
+        System.out.println("Contenido del archivo:");
+        System.out.println(contenido);
+    }
+}
